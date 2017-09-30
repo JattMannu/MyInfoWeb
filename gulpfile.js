f1c90f07278831985f9479086313b523d5fdf7cd
JattMannu <manpreet_firewall@yahoo.com.sg>
2017-09-30 20:56:27 +0800

    Init Checkin

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+//npm install -g gulp
+//npm install --save gulp gulp-live-server
+
+const gulp = require('gulp');
+const LiveServer = require('gulp-live-server');
+const browserSync = require('browser-sync');
+const browserify = require('browserify');
+const reactify = require('reactify');
+const source = require('vinyl-source-stream');
+
+gulp.task('live-server', function(){
+    //This code gets called when live-server is ran via cli
+    const server = new LiveServer('./server/main.js');
+    server.start();
+});
+
+gulp.task('serve' , ['live-server' ], function(){
+    browserSync.init(null , {
+        proxy : 'http://localhost:9999',
+        port:9990
+    });
+});
+
+//This function convert a JSX into a JS
+gulp.task('bundle' , function(){
+    return browserify({
+        entries : 'app/main.jsx',
+        debug : true,
+    })
+    .transform(reactify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./.tmp'));
+});
+
